b5ff27c2ccd626c7eec0306a1e96abb895e9cfd2
Sagar Singichetti <singichs@umich.edu>
2022-06-30 15:49:42 -0400

    Initial Commit

---
+++ KitmanLabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KitmanLabs
+//
+//  Created by Sagar Singichetti on 6/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
